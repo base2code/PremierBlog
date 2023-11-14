2b3068aea02f954429a99bebae5f35d0f0dcd7d2
base2code <niklas.grenningloh@gmx.de>
2023-11-14 22:23:55 +0100

    initial commit

---
+++ src/main/java/de/base2code/blog/PremierApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package de.base2code.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremierApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/base2code/blog/PremierApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.base2code.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
