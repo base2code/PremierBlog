6075ccd62f2581bca308da4478d507fbbe5c865d
base2code <niklas.grenningloh@gmx.de>
2023-11-16 10:49:07 +0100

    Extended the DTO with date objects

--- src/main/java/de/base2code/blog/dto/web/posts/ExternalPostDto.java
+++ src/main/java/de/base2code/blog/dto/web/posts/ExternalPostDto.java
@@ -4,10 +4,14 @@ import de.base2code.blog.dto.web.user.PublicUserDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 public class ExternalPostDto {
     private String id;
+    private Date postedAt;
+    private Date updatedAt;
     private String title;
     private String content;
     private PublicUserDto author;
