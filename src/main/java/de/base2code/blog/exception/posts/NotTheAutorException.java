2f21dd0850efa6eba87a4bbf6cafc2f7a40b6b84
base2code <niklas.grenningloh@gmx.de>
2023-11-18 09:29:16 +0100

    Fixed typo

--- src/main/java/de/base2code/blog/exception/posts/NotTheAutorException.java
+++ src/main/java/de/base2code/blog/exception/posts/NotTheAutorException.java
@@ -2,6 +2,6 @@ package de.base2code.blog.exception.posts;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(reason = "Your are not the author", code = org.springframework.http.HttpStatus.UNAUTHORIZED)
+@ResponseStatus(reason = "You are not the author", code = org.springframework.http.HttpStatus.UNAUTHORIZED)
 public class NotTheAutorException extends Exception {
 }
